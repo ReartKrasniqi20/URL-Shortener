9db7ae6f92779e4d5aa0677677cadd4e8f2027ed
reartkrasniqi <reart.krasniqi@student.uni-pr.edu>
2025-09-25 17:38:54 +0200

    Add database connection file (db.php) using .env variables

---
+++ src/db.php
@@ -0,0 +1,15 @@
+<?php
+
+$env = parse_ini_file(__DIR__ . '/.env');
+
+$host = $env['DB_HOST'];
+$db_name = $env['DB_NAME'];
+$username = $env['DB_USER'];
+$password = $env['DB_PASS'];
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
